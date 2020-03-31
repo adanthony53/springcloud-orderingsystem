70d2455f97ab7f40ebcf0e1af2a114b54459e7f3
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-03-31 15:25:21 -0700

    Initial commit with Eureka Server, Config Server, Order Service

---
+++ order/src/main/java/com/example/controller/OrderHandler.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderHandler {
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index() {
+        return "current port: " + this.port;
+    }
+}
